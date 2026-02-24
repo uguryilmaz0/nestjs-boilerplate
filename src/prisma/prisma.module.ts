d676868d1fb49a390ea2dfd563532a8230817318
uguryilmaz0 <yugur464@icloud.com>
2026-02-24 23:02:07 +0300

    chore(config): enable strict mode and resolve jwt-strategy type issues

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,9 +1,9 @@
 import { Module, Global } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
-@Global() // 🌍 Bu, PrismaService'i tüm uygulama için "görünür" kılar.
+@Global() // Bu, PrismaService'i tüm uygulama için "görünür" kılar. / This makes PrismaService "visible" across the entire application.
 @Module({
   providers: [PrismaService],
-  exports: [PrismaService], // 🔑 Dışarıya açıyoruz ki AuthService buna ulaşabilsin.
+  exports: [PrismaService], // Dışarıya açıyoruz ki AuthService buna ulaşabilsin. / We export it so that AuthService can access it.
 })
-export class PrismaModule {}
+export class PrismaModule { }
